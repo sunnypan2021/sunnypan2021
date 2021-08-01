b81b6a64819dea0d5a525ac6c4761c1c408dc029
sunny pandey <sunny.20jan@gmail.com>
2021-08-01 20:42:11 +0530

    flask app heroku

--- Pickle/load_model.py
+++
@@ -1,8 +0,0 @@
-import pickle
-# load the model
-loaded_model=pickle.load(open('dib_79.pkl' , 'rb'))
-pred=loaded_model.predict([[10,12,45,32,55,22,45,22]])
-if pred[0]==1:
-    print("diabetic")
-else:
-    print("nondiabetic")
